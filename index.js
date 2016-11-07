ced84579faf04cecf0d0e48355d35335cf3a242a
Roman Teplov <teplov@gmail.com>
2016-11-07 20:50:07 +0300

    ADD CORS

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 var express = require('express');
 let app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
   let a = +req.query.a || 0;
